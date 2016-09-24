d794a2d83b2cad462f4017ab883ea5e9f74a0c37
kendall weihe <kendallweihe@gmail.com>
2016-09-24 14:15:26 -0400

    added python script which generate random guesses

--- prog2/bomb93/test_script.py
+++ prog2/bomb93/test_script.py
@@ -1,11 +1,14 @@
 import subprocess as sp
 import numpy as np
+import pdb
 
 combinations = []
 while 1:
 
 	found = False
-	while found:
+	line = ""
+#	pdb.set_trace()
+	while not found:
 		num_1 = np.random.randint(20)
 		num_2 = np.random.randint(20)
 		num_3 = np.random.randint(20)
@@ -14,10 +17,13 @@ while 1:
 		num_6 = np.random.randint(20)
 
 		line = str(num_1) + " " + str(num_2) + " " + str(num_3) + " " + str(num_4) + " " + str(num_5) + " " + str(num_6)
+#		print combinations
+#		print line
 		if line not in combinations:
 			combinations.append(line)
 			found = True
 
-	regex = "2s/.*/" + line + "/"
-	sp.call(["sed", "-i", regex, "answers_2"])
-	sp.call(["gdb", "--command=gdb_command_2", "bomb"])
+			regex = "2s/.*/" + line + "/"
+			print regex
+			sp.call(["sed", "-i", regex, "answers_2.txt"])
+			sp.call(["gdb", "--command=gdb_commands_2", "bomb"])
